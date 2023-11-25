3ff233433c5e0dcbd674fb79a61eb4d90d0d5f6b
David Requeno <davidrequeno52@gmail.com>
2023-11-24 22:03:19 -0600

    hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen bg-cover bg-center bg-no-repeat mx-auto' style={{ backgroundImage: `url('/path/to/computer_image.jpg')` }}>
+      <div className='absolute inset-0 top-20 max-w-7xl mx-auto px-5 flex flex-row items-start gap-5'>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-blue-600' />
+          <div className='w-1 sm:h-80 h-40 bg-gradient-to-b from-blue-600 to-transparent' />
+        </div>
+
+        <div>
+          <h1 className='font-black text-white lg:text-6xl sm:text-5xl xs:text-4xl text-3xl lg:leading-12 mt-2'>
+            Hi, I'm <span className='text-blue-600'>David</span>
+          </h1>
+          <p className='text-gray-300 font-medium lg:text-xl sm:text-lg xs:text-base text-sm lg:leading-10 mt-2'>
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces, and web applications
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
