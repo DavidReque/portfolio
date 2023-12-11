364bceb224832c8b3e8e4ee7d44b02722a70cf47
David Requeno <davidrequeno52@gmail.com>
2023-12-10 22:24:00 -0600

    types

--- src/app/types/types.d.ts
+++ src/app/types/types.d.ts
@@ -1,5 +1,4 @@
 export interface Project {
-<<<<<<< HEAD
     title: string;
     description: string;
     technologies: string[];
@@ -7,13 +6,3 @@ export interface Project {
     githubLink: string;
     site: string;
   }
-  
-=======
-  title: string;
-  description: string;
-  technologies: string[];
-  imageUrl: string;
-  githubLink: string;
-  site: string;
-}
->>>>>>> c498914aad2ea25c4f4f1af1be2cc6e6b41544f5
