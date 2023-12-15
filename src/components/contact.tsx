c03be65b3b7123daf3046be9ee5069c31090ad64
David Requeno <davidrequeno52@gmail.com>
2023-12-14 20:14:53 -0600

    ball

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,16 +2,21 @@
 
 import { Button, Input, Textarea } from '@nextui-org/react'
 import React from 'react'
+import { motion } from "framer-motion";
+import EarthCanvas from './canvas/EarthCanvas';
+import { slideIn } from '@/utils/motion';
 
 export const Contact = () => {
   return (
-    <section className="py-16">
-      <div className="max-w-5xl mx-auto px-4">
+    <section className='text-gray-300 font-light mt-32'>
+      <div className="lg:max-w-4xl lg:mx-auto">
       <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">Contact</p>
-        <h2 className="text-3xl sm:text-4xl font-bold text-gray-200 mb-8">Contact Us</h2>
+      <h2 className={`text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]`}>
+        Contact
+        </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           <div style={{ backgroundColor: '#151030' }} className="p-6 rounded-lg shadow-md">
-            <h3 className="text-2xl font-semibold text-gray-200 mb-4">Send us a message</h3>
+            <h3 className="text-2xl font-semibold text-gray-200 mb-4">Send me a message</h3>
             <form>
               <div className="mb-4">
               <Input 
@@ -26,7 +31,7 @@ export const Contact = () => {
                 key='primary'
                 type='email' 
                 color='primary'
-                placeholder="Enter your mail"
+                placeholder="Enter your email"
                 className="w-full"
                  />
               </div>
@@ -42,6 +47,12 @@ export const Contact = () => {
               </Button>
             </form>
           </div>
+          <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
         </div>
       </div>
     </section>
