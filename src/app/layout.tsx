c7b27e2801330c3b2a310ef15f846f8e0e7526d5
David Requeno <davidrequeno52@gmail.com>
2023-12-17 22:36:40 -0600

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'David Requeno',
   description: 'David Requeno',
+  icons: {
+    icon: ['/logo.png']
+  }
 }
 
 export default function RootLayout({
